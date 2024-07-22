9f2640b23c21275002fa04a8c5ebd6fddf1ef8c5
owdax <omid.dolatkia@gmail.com>
2024-07-22 04:17:00 +0330

    nvim
    * lazy plugin manager
    * initial remaps
    * catpuccin theme

---
+++ nvim/lua/set-remaps.lua
@@ -0,0 +1,31 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.colorcolumn = "120"
